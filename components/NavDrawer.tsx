afeb3640058fdfb5545721fefdc2f6676d354c11
khai-rule <qai-rule@hotmail.com>
2026-07-12 16:54:40 +0800

    feat: update Upload nav item href to point to /upload

--- components/NavDrawer.tsx
+++ components/NavDrawer.tsx
@@ -9,7 +9,7 @@ import { animatePageOut } from "@/animations";
 
 const NAV_ITEMS = [
   {
-    href: "/",
+    href: "/upload",
     label: "Upload",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
